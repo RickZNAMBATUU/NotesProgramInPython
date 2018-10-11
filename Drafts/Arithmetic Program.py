5d2c0bab69b987bb092b5223a3b4295ce2f8824e
Frederick Castaneda Jr <frederick.castanedajr04@gmail.com>
2018-10-11 00:13:26 -0800

    Added Drafts folder. 10/11/2018

---
+++ Drafts/Arithmetic Program.py
@@ -0,0 +1,48 @@
+class Arithmetic:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def addition(self):
+        z = self.x + self.y
+        print("Answer is: ")
+        return z
+
+    def subtraction(self):
+        z = self.x - self.y
+        print("Answer is: ")
+        return z
+    
+    def multiplication(self):
+        z = self.x * self.y
+        print("Answer is: ")
+        return z
+    
+    def division(self):
+        z = self.x // self.y
+        print("Answer is: ")
+        return z
+
+userOpt = int(input("""Select what arithmetic operation you want to use:
+1.) Addition
+2.) Subtraction
+3.) Multiplication
+4.) Division
+Answer: """))
+
+userInp1 = int(input("Enter first number: "))
+userInp2 = int(input("Enter second number: "))
+
+app = Arithmetic(userInp1, userInp2)
+
+if userOpt == 1:
+    print(app.addition())
+elif userOpt == 2:
+    print(app.subtraction())
+elif userOpt == 3:
+    print(app.multiplication())
+elif userOpt == 4:
+    print(app.division())
+else:
+    print("Invalid input! Please try again.")
+    exit()
